251e8be3db34d32ce78292539d336dfd33d9732d
Julie Graceffa <julie.graceffa@gmail.com>
2016-09-04 14:56:19 -0400

    Update basic_betch_test.rb

--- basic_betch/test/basic_betch_test.rb
+++ basic_betch/test/basic_betch_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class BasicBetchTest < Minitest::Test
+class BasicbetchTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::BasicBetch::VERSION
+    refute_nil ::Basicbetch::VERSION
   end
 
   def test_it_does_something_useful
